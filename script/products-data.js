c9cd72e7468f9e0f6fdc39121c70a2f76378fb62
Alexey Bondarev <6933666@mail.ru>
2026-05-17 12:27:21 +0300

    Реализация 2026

--- script/products-data.js
+++ script/products-data.js
@@ -6994,7 +6994,7 @@ const soldProducts = {
     'zucchinis': [3, 5, 6, 8, 9, 10], //кабачки
     'eggplants': [2, 3], //баклажаны
     'pumpkins': [2, 3, 4, 6, 7], //тыквы
-    'watermelons': [1, 2, 3, 6, 7], //арбузы
+    'watermelons': [1, 2, 3, 4, 5, 6, 7, 8], //арбузы
     'melons': [1, 3], // дыни
     'cabbages': [], //капуста
     'flowers': []
